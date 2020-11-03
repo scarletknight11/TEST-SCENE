74ce5a363de697a29fba212d468c2afd23bef7a1
Sanjit Singh <sanjitpsingh@yahoo.com>
2020-11-02 21:11:27 -0500

    simple Unity example for begineers

---
+++ Assets/Scripts/hello.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hello : MonoBehaviour {
+
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision coll)
+    {
+        if(coll.gameObject.tag =="Floor")
+        {
+            cube.SetActive(false);
+        }
+    }
+}
